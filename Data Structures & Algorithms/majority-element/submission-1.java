93a601cd3e1fb1ee7fd9c10aea9bd3d786bc15bb
Nandhakkumar <84634050+orzTHUNDER@users.noreply.github.com>
2026-05-11 09:16:15 +0530

    Add: majority-element - submission-1

---
+++ Data Structures & Algorithms/majority-element/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+
+        int canditate = nums[0];
+        int cnt = 1;
+
+        for(int num: nums) {
+            cnt -= (num == canditate ? -1 : 1);
+            if(cnt <  0) {
+                canditate = num;
+                cnt = 1;
+            }
+        }
+        // for(int i = 1; i < nums.length; i++) {
+        //     if(nums[i] == canditate) cnt++;
+        //     else
+        //         cnt--;
+            
+        //     if(cnt < 0) {
+        //         canditate = nums[i];
+        //         cnt = 1;
+        //     }
+
+        // }
+        return canditate;
+    }
+}
