9ceeb2d46b3cfb7db59f0f0484c499a429ebd04a
Nandhakkumar <84634050+orzTHUNDER@users.noreply.github.com>
2026-05-12 08:23:07 +0530

    Add: sqrtx - submission-9

---
+++ Data Structures & Algorithms/sqrtx/submission-9.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int mySqrt(int x) {
+
+
+        int l = 0, r = x;
+
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if ((long) m * m > x) {
+                r = m - 1;
+            } else if ((long) m * m < x) {
+                l = m + 1;
+            } else {
+                return m;
+            }
+        }
+
+        return r;
+        
+    }
+}
